0dfb949fe21d8173526a481ec13bfaf246e1c140
ReshetnikovIvan121 <ivanreshetnikov235@gmail.com>
2023-06-26 19:53:41 +0700

    edit sort

--- sorts/bubble/bubblesort.c
+++ sorts/bubble/bubblesort.c
@@ -5,13 +5,13 @@
 int bubble(int arr[], int size){
     int i, j, temp;
     for(i = 0; i < size; i++){
-	for(j = 0; j < size - 1; j++){
-	    if(arr[j] > arr[j + 1]){
-		temp = arr[j];
-		arr[j] = arr[j + 1];
-		arr[j + 1] = temp;
+	    for(j = 0; j < size - 1; j++){
+	        if(arr[j] > arr[j + 1]){
+		        temp = arr[j];
+		        arr[j] = arr[j + 1];
+		        arr[j + 1] = temp;
+	        }
 	    }
-	}
     }
     return arr[size];
 }
