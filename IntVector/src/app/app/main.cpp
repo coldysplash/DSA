cdcc51f032542de5d5f746681d5c7734f839a0f4
Ivan <ivanreshetnikov235@gmail.com>
2024-02-11 20:32:48 +0700

    add rule of 5 and new tests

--- IntVector/src/app/app/main.cpp
+++ IntVector/src/app/app/main.cpp
@@ -1,20 +1,10 @@
 #include "../../libvector/IntVector.hpp"
 
 #include <iostream>
-#include <vector>
 
 int main() {
 
   using namespace intvector;
-
-  IntVector vec1{2, 0, 0, 0};
-  IntVector vec2{0, 1, 2, 3};
-
-  bool equal = vec1 == vec2;
-  bool equal2 = vec1 < vec2;
-
-  std::cout << equal << '\n';
-  std::cout << equal2 << '\n';
 
   return 0;
 }
--- IntVector/src/libvector/IntVector.hpp
+++ IntVector/src/libvector/IntVector.hpp
@@ -15,23 +15,26 @@ public:
   IntVector();
   IntVector(const std::initializer_list<int> &init_list);
 
-  /* capacity */
+  /* Rule of 5 */
+  IntVector(const IntVector &v_copy);
+  IntVector(IntVector &&v_copy);
+  IntVector &operator=(const IntVector &vec);
+  IntVector &operator=(IntVector &&vec);
+  ~IntVector();
+
   size_t size() const;
   size_t capacity() const;
-  void reserve(size_t n_capa);
-
-  void push_back(int value);
 
   /* accessors to elements */
   int *begin() const;
   int *end() const;
   int &operator[](const size_t index) noexcept;
-
-  ~IntVector() { delete[] arr; }
 };
 
+/* Standard constructor */
 IntVector::IntVector() : arr(nullptr), size_(0), capacity_(0) {}
 
+/* std::initializer_list constructor */
 IntVector::IntVector(const std::initializer_list<int> &init_list)
     : arr(new int[init_list.size()]), size_(init_list.size()),
       capacity_(init_list.size()) {
@@ -43,40 +46,74 @@ IntVector::IntVector(const std::initializer_list<int> &init_list)
   }
 }
 
+/* Copy constructor */
+IntVector::IntVector(const IntVector &v_copy) {
+  arr = new int[v_copy.capacity_];
+  size_ = v_copy.size_;
+  capacity_ = v_copy.capacity_;
+  for (size_t i = 0; i != size_; ++i) {
+    arr[i] = v_copy.arr[i];
+  }
+}
+
+/* Move constructor */
+IntVector::IntVector(IntVector &&v_copy)
+    : size_(std::move(v_copy.size_)), capacity_(std::move(v_copy.capacity_)) {
+
+  arr = std::move(v_copy.arr);
+
+  v_copy.arr = nullptr;
+  v_copy.size_ = 0;
+  v_copy.capacity_ = 0;
+}
+
+/* Copy assignment operator */
+IntVector &IntVector::operator=(const IntVector &vec) {
+  if (vec.arr == arr) {
+    return *this;
+  }
+
+  delete[] arr;
+  arr = new int[vec.capacity_];
+
+  size_ = vec.size_;
+  capacity_ = vec.capacity_;
+
+  for (size_t i = 0; i < size_; ++i) {
+    arr[i] = vec.arr[i];
+  }
+
+  return *this;
+}
+
+/* Move assignment operator */
+IntVector &IntVector::operator=(IntVector &&vec) {
+  if (vec.arr == arr) {
+    return *this;
+  }
+
+  std::swap(arr, vec.arr);
+  std::swap(size_, vec.size_);
+  std::swap(capacity_, vec.capacity_);
+
+  vec.arr = nullptr;
+  vec.size_ = 0;
+  vec.capacity_ = 0;
+
+  return *this;
+}
+
+/* Destructor */
+IntVector::~IntVector() { delete[] arr; }
+
 size_t IntVector::size() const { return size_; }
 size_t IntVector::capacity() const { return capacity_; }
 
-void IntVector::reserve(size_t n_capa) {
-  if (n_capa > capacity_) {
-    int *newarr = new int[n_capa];
-    for (size_t i = 0; i < size_; ++i) {
-      newarr[i] = arr[i];
-    }
-    delete[] arr;
-
-    arr = newarr;
-    capacity_ = n_capa;
-
-    delete[] newarr;
-  }
-}
-
-void IntVector::push_back(int value) {
-  if (capacity_ == size_) {
-    reserve(2 * size_);
-  }
-  arr[size_] = value;
-  ++size_;
-}
-
 int *IntVector::begin() const { return arr; }
-
 int *IntVector::end() const { return (arr + size_); }
-
 int &IntVector::operator[](const size_t index) noexcept { return arr[index]; }
 
 /* comparison operators */
-
 inline bool operator==(const IntVector &lhs, const IntVector &rhs) {
   return (
       lhs.size() == rhs.size() &&
