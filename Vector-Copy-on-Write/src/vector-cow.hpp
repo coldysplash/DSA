b0ce6df78b7532e81045ab213715a2fec6a609a4
Ivan <ivanreshetnikov235@gmail.com>
2024-05-08 17:49:26 +0700

    add 'vector-copy-on-write' project

---
+++ Vector-Copy-on-Write/src/vector-cow.hpp
@@ -0,0 +1,460 @@
+#pragma once
+
+#include <cstddef>
+#include <initializer_list>
+#include <iterator>
+#include <memory>
+#include <stdexcept>
+#include <utility>
+
+namespace vector_cow {
+
+template <typename T> class Vector {
+public:
+  using value_type = T;
+  using size_type = size_t;
+  using difference_type = std::ptrdiff_t;
+  using reference = value_type &;
+  using const_reference = const value_type &;
+  using pointer = value_type *;
+  using const_pointer = const value_type *;
+
+  // Iterator
+  template <typename It> class Iterator {
+  public:
+    using difference_type = std::ptrdiff_t;
+    using value_type = It;
+    using reference = value_type &;
+    using pointer = value_type *;
+    using iterator_category = std::random_access_iterator_tag;
+
+    explicit Iterator(Vector::pointer arr) : arr_(arr) {}
+    auto &operator++() noexcept {
+      ++arr_;
+      return *this;
+    }
+    auto operator++(int) noexcept {
+      auto temp = *this;
+      ++(*this);
+      return temp;
+    }
+    auto &operator--() noexcept {
+      --arr_;
+      return *this;
+    }
+    auto operator--(int) noexcept {
+      auto temp = *this;
+      --(*this);
+      return temp;
+    }
+
+    reference operator*() const noexcept { return *arr_; }
+    pointer operator->() const noexcept { return arr_; }
+
+    bool operator==(const Iterator &other) const noexcept {
+      return arr_ == other.arr_;
+    }
+    bool operator!=(const Iterator &other) const noexcept {
+      return !(arr_ == other.arr_);
+    }
+    friend bool operator<(const Iterator &lhs, const Iterator &rhs) noexcept {
+      return (rhs - lhs > 0);
+    }
+    friend bool operator>(const Iterator &lhs, const Iterator &rhs) noexcept {
+      return rhs < lhs;
+    }
+    friend bool operator<=(const Iterator &lhs, const Iterator &rhs) noexcept {
+      return !(rhs < lhs);
+    }
+    friend bool operator>=(const Iterator &lhs, const Iterator &rhs) noexcept {
+      return !(lhs < rhs);
+    }
+
+    reference operator[](difference_type n) const { return *(arr_ + n); }
+    Iterator &operator+=(difference_type n) {
+      if (n >= 0)
+        while (n--)
+          ++arr_;
+      else
+        while (n++)
+          --arr_;
+      return *this;
+    }
+    Iterator &operator-=(difference_type n) { return *this += -n; }
+
+    friend Iterator operator+(Iterator it, difference_type n) {
+      return it += n;
+    }
+    friend Iterator operator+(difference_type n, Iterator it) { return it + n; }
+    friend Iterator operator-(Iterator it, difference_type n) {
+      it -= n;
+      return it;
+    }
+    friend difference_type operator-(const Iterator &lhs, const Iterator &rhs) {
+      return lhs.arr_ - rhs.arr_;
+    }
+
+  private:
+    Vector::pointer arr_;
+  };
+
+  using iterator = Iterator<value_type>;
+  using const_iterator = const Iterator<value_type>;
+  using reverse_iterator = std::reverse_iterator<iterator>;
+  using const_reverse_iterator = std::reverse_iterator<const_iterator>;
+
+  iterator begin() noexcept {
+    if (data_.use_count() > 1) {
+      detach();
+    }
+    return iterator(data_->begin_);
+  }
+  iterator end() noexcept {
+    if (data_.use_count() > 1) {
+      detach();
+    }
+    return iterator(data_->begin_ + size());
+  }
+  const_iterator begin() const noexcept {
+    return const_iterator(data_->begin_);
+  }
+  const_iterator end() const noexcept {
+    return const_iterator(data_->begin_ + size());
+  }
+  const_iterator cbegin() const noexcept {
+    return const_iterator(data_->begin_);
+  }
+  const_iterator cend() const noexcept {
+    return const_iterator(data_->begin_ + size());
+  }
+  reverse_iterator rbegin() noexcept { return reverse_iterator(end()); }
+  reverse_iterator rend() noexcept { return reverse_iterator(begin()); }
+  const_reverse_iterator crbegin() const noexcept {
+    return const_reverse_iterator(cend());
+  }
+  const_reverse_iterator crend() const noexcept {
+    return const_reverse_iterator(cbegin());
+  }
+
+  Vector() { data_ = std::make_shared<Storage<value_type>>(0, 0); }
+
+  explicit Vector(size_type size)
+      : data_(std::make_shared<Storage<value_type>>(size, size)) {
+    size_t current = 0;
+    try {
+      for (; current < size; ++current) {
+        new (data_->begin_ + current) T(0);
+      }
+    } catch (const std::exception &e) {
+      for (size_t i = 0; i < current; ++i) {
+        data_->begin_[i].~T();
+        operator delete(data_->begin_);
+      }
+      throw;
+    }
+  }
+
+  Vector(size_type size, const_reference value)
+      : data_(std::make_shared<Storage<value_type>>(size, size)) {
+    size_t current = 0;
+    try {
+      for (; current < size; ++current) {
+        new (data_->begin_ + current) T(value);
+      }
+    } catch (const std::exception &e) {
+      for (size_t i = 0; i < current; ++i) {
+        data_->begin_[i].~T();
+        operator delete(data_->begin_);
+      }
+      throw;
+    }
+  }
+
+  explicit Vector(std::initializer_list<T> items)
+      : data_(
+            std::make_shared<Storage<value_type>>(items.size(), items.size())) {
+    std::uninitialized_move(items.begin(), items.end(), data_->begin_);
+  }
+
+  Vector(iterator first, iterator end) {
+    size_t size = end - first;
+    data_ = std::make_shared<Storage<value_type>>(size, size);
+    size_t current = 0;
+    try {
+      for (; current < size; ++current, ++first) {
+        new (data_->begin_ + current) T(*first);
+      }
+    } catch (const std::exception &e) {
+      for (size_t i = 0; i < current; ++i) {
+        data_->begin_[i].~T();
+        operator delete(data_->begin_);
+      }
+      throw;
+    }
+  }
+
+  void reserve(size_type new_capa) {
+    if (new_capa > capacity()) {
+      auto tmp = std::make_shared<Storage<value_type>>(size(), new_capa);
+      size_t current = 0;
+      try {
+        for (; current < size(); ++current) {
+          new (tmp->begin_ + current) T(*(data_->begin_ + current));
+        }
+      } catch (const std::exception &e) {
+        for (size_t i = 0; i < current; ++i) {
+          tmp->begin_[i].~T();
+        }
+        operator delete(tmp->begin_);
+        throw;
+      }
+      data_.swap(tmp);
+    }
+  }
+
+  void push_back(const_reference value) {
+    if (data_.use_count() > 1 && size() < capacity()) {
+      detach();
+    }
+    if (size() == 0) {
+      reserve(1);
+    } else if (size() == capacity()) {
+      reserve(size() * 2);
+    }
+    try {
+      new (data_->begin_ + size()) T(value);
+    } catch (const std::exception &e) {
+      data_->begin_[size()].~T();
+      throw;
+    }
+    ++data_->size_;
+  }
+
+  void resize(size_type count) {
+    if (count > size()) {
+      if (count > capacity()) {
+        reserve(count);
+      }
+      size_t current = size();
+      try {
+        for (; current < count; ++current) {
+          new (data_->begin_ + current) T();
+        }
+      } catch (const std::exception &e) {
+        for (size_t i = 0; i < current; ++i) {
+          data_->begin_[i].~T();
+          operator delete(data_->begin_);
+        }
+        throw;
+      }
+    } else if (count < size()) {
+      for (size_t i = size(); i > count; --i) {
+        data_->begin_[i].~T();
+      }
+    } else {
+      return;
+    }
+    data_->size_ = count;
+  }
+
+  reference operator[](size_type pos) {
+    if (data_.use_count() > 1) {
+      detach();
+    }
+    return data_->begin_[pos];
+  }
+
+  const_reference operator[](size_type pos) const { return data_->begin_[pos]; }
+
+  reference at(size_type pos) {
+    if (pos >= size()) {
+      throw std::out_of_range("Out of range!");
+    }
+    if (data_.use_count() > 1) {
+      detach();
+    }
+    return data_->begin_[pos];
+  }
+
+  const_reference at(size_type pos) const {
+    if (pos >= size()) {
+      throw std::out_of_range("Out of range!");
+    }
+    return data_->begin_[pos];
+  }
+
+  size_type size() const noexcept { return data_->size_; }
+  size_type capacity() const noexcept { return data_->capacity_; }
+
+  bool empty() const noexcept { return (size() == 0); }
+
+  void shrink_to_fit() {
+    if (capacity() > size()) {
+      auto tmp = std::make_shared<Storage<value_type>>(size(), size());
+      size_t current = 0;
+      try {
+        for (; current < size(); ++current) {
+          new (tmp->begin_ + current) T(*(data_->begin_ + current));
+        }
+      } catch (const std::exception &e) {
+        for (size_t i = 0; i < current; ++i) {
+          tmp->begin_[i].~T();
+        }
+        operator delete(tmp->begin_);
+        throw;
+      }
+      data_.swap(tmp);
+    }
+  }
+
+  iterator insert(const_iterator pos, const T &value) {
+    const size_t idx = pos - cbegin();
+
+    if (pos == cend()) {
+      push_back(value);
+    } else {
+      push_back(value);
+      for (auto i = size() - 1; i != idx; --i) {
+        data_->begin_[i] = data_->begin_[i - 1];
+      }
+      data_->begin_[idx] = value;
+    }
+
+    return cbegin() + idx;
+  }
+
+  iterator insert(const_iterator pos, size_type count, const T &value) {
+    if (count == 0) {
+      return pos;
+    }
+    const size_t idx = pos - cbegin();
+
+    if (pos == cend()) {
+      for (size_t i = 0; i < count; ++i) {
+        push_back(value);
+      }
+    } else {
+      for (size_t i = 0; i != count; ++i) {
+        auto current_pos = begin() + idx;
+        insert(current_pos, value);
+      }
+    }
+    return begin() + idx;
+  }
+
+  iterator insert(const_iterator pos, iterator first, iterator last) {
+    if (first == last) {
+      return pos;
+    }
+    const size_t idx = pos - cbegin();
+    const size_t count = last - first;
+
+    if (pos == cend()) {
+      auto cur = first;
+      for (size_t i = 0; i < count; ++i, ++cur) {
+        push_back(*cur);
+      }
+    } else {
+      for (size_t i = 0; i < count; ++i, ++first) {
+        auto current_pos = begin() + idx;
+        insert(current_pos, *first);
+      }
+    }
+    return begin() + idx;
+  }
+
+  iterator insert(const_iterator pos, std::initializer_list<T> ilist) {
+    if (ilist.size() == 0) {
+      return pos;
+    }
+    const size_t idx = pos - cbegin();
+    const size_t count = ilist.size();
+
+    if (pos == cend()) {
+      auto cur = ilist.begin();
+      for (size_t i = 0; i < count; ++i, ++cur) {
+        push_back(*cur);
+      }
+    } else {
+      auto item = ilist.begin();
+      for (size_t i = 0; i < count; ++i, ++item) {
+        auto current_pos = begin() + idx;
+        insert(current_pos, *item);
+      }
+    }
+    return begin() + idx;
+  }
+
+  iterator erase(const_iterator pos) {
+    if (pos == cend()) {
+      return pos;
+    }
+    const size_t idx = pos - cbegin();
+    const size_t last = cend() - cbegin();
+    for (auto item = pos; item != end() - 1; ++item) {
+      std::swap(*item, *(item + 1));
+    }
+    data_->begin_[last].~T();
+    --data_->size_;
+    return cbegin() + idx;
+  }
+
+  iterator erase(const_iterator first, const_iterator last) {
+    if (first == last) {
+      return last;
+    }
+    const size_t idx = first - cbegin();
+    const size_t count = last - first;
+    for (size_t i = 0; i < count; ++i) {
+      erase(first);
+    }
+    return cbegin() + idx;
+  }
+
+  size_t count() const { return data_.use_count(); }
+
+private:
+  void detach() {
+    auto tmp = std::make_shared<Storage<value_type>>(size(), capacity());
+    size_t current = 0;
+    try {
+      for (; current < size(); ++current) {
+        new (tmp->begin_ + current) T(*(data_->begin_ + current));
+      }
+    } catch (const std::exception &e) {
+      for (size_t i = 0; i < current; ++i) {
+        tmp->begin_[i].~T();
+      }
+      operator delete(tmp->begin_);
+      throw;
+    }
+    data_.swap(tmp);
+  }
+
+  template <typename S> struct Storage {
+    S *begin_ = nullptr;
+    size_t size_ = 0;
+    size_t capacity_ = 0;
+
+    Storage(size_t size, size_t capa) : size_(size), capacity_(capa) {
+      begin_ = static_cast<S *>(operator new(sizeof(S) * capacity_));
+    }
+    ~Storage() {
+      for (std::size_t i = 0; i < size_; ++i) {
+        begin_[i].~S();
+      }
+      operator delete(begin_);
+    }
+  };
+
+  std::shared_ptr<Storage<value_type>> data_;
+};
+
+template <typename T>
+inline bool operator==(const Vector<T> &lhs, const Vector<T> &rhs) {
+  return (
+      lhs.size() == rhs.size() &&
+      std::equal(lhs.begin(), lhs.end(), rhs.begin()));
+}
+
+} // namespace vector_cow
---
+++ Vector-Copy-on-Write/test/vec-cow-test.cpp
@@ -0,0 +1,255 @@
+#include <vector-cow.hpp>
+
+#include <gtest/gtest.h>
+
+#include <iostream>
+#include <string>
+
+TEST(test_vector, initializer_constructor) {
+  vector_cow::Vector<std::string> v{"hello", "hello"};
+  vector_cow::Vector<std::string> v_2(2, "hello");
+  ASSERT_EQ(v, v_2);
+  ASSERT_EQ(v.size(), 2);
+  ASSERT_EQ(v.capacity(), 2);
+}
+
+TEST(test_vector, size_constructor) {
+  vector_cow::Vector<int> v(5);
+  vector_cow::Vector<int> v_2{0, 0, 0, 0, 0};
+  ASSERT_EQ(v, v_2);
+}
+
+TEST(test_vector, constructor_from_range) {
+  vector_cow::Vector<int> v{5, 6, 7, 8};
+  vector_cow::Vector<int> v_2(v.begin(), v.end());
+  ASSERT_EQ(v, v_2);
+}
+
+TEST(test_vector, detach) {
+  {
+    vector_cow::Vector<int> v{5, 6, 7, 8};
+    vector_cow::Vector<int> v_2(v);
+    ASSERT_EQ(v.count(), 2);
+    ASSERT_EQ(v_2.count(), 2);
+    v_2.push_back(3);
+    ASSERT_EQ(v.count(), 1);
+    ASSERT_EQ(v_2.count(), 1);
+  }
+  {
+    vector_cow::Vector<int> v{5, 6, 7, 8};
+    vector_cow::Vector<int> v_2 = v;
+    ASSERT_EQ(v.count(), 2);
+    ASSERT_EQ(v_2.count(), 2);
+    v_2.insert(v_2.begin(), 3);
+    ASSERT_EQ(v.count(), 1);
+    ASSERT_EQ(v_2.count(), 1);
+  }
+}
+
+TEST(test_vector, reserve) {
+  vector_cow::Vector<std::string> v;
+  v.reserve(5);
+  ASSERT_EQ(v.capacity(), 5);
+  ASSERT_EQ(v.size(), 0);
+}
+
+TEST(test_vector, push_back) {
+  vector_cow::Vector<int> v;
+  v.push_back(2);
+  ASSERT_EQ(*v.begin(), 2);
+}
+
+TEST(test_vector, resize) {
+  vector_cow::Vector<int> v(2, 5);
+  v.resize(5);
+  vector_cow::Vector<int> expect{5, 5, 0, 0, 0};
+  ASSERT_EQ(v, expect);
+  v.resize(0);
+  ASSERT_EQ(v.size(), 0);
+  ASSERT_EQ(v.capacity(), 5);
+  {
+    vector_cow::Vector<std::string> v(2, "hello");
+    v.resize(5);
+  }
+}
+
+TEST(test_vector, access_operator) {
+  {
+    vector_cow::Vector<int> v(3, 2);
+    v[2] = 4;
+    vector_cow::Vector<int> expect{2, 2, 4};
+    ASSERT_EQ(v, expect);
+  }
+  {
+    const vector_cow::Vector<int> v(4, 2);
+    // std::cout << '\n' << v[2];
+  }
+}
+
+TEST(test_vector, access_at) {
+  {
+    vector_cow::Vector<int> v(3, 2);
+    v.at(2) = 4;
+    vector_cow::Vector<int> expect{2, 2, 4};
+    ASSERT_EQ(v, expect);
+  }
+  { const vector_cow::Vector<int> v(4, 2); }
+}
+
+TEST(test_vector, empty) {
+  vector_cow::Vector<int> v;
+  ASSERT_EQ(v.empty(), true);
+  v.push_back(2);
+  ASSERT_EQ(v.empty(), false);
+}
+
+TEST(test_vector, shrink_to_fit) {
+  vector_cow::Vector<int> v;
+  v.resize(100);
+  v.resize(50);
+  v.shrink_to_fit();
+  ASSERT_EQ(v.capacity(), 50);
+}
+
+TEST(test_vector, iterator) {
+  using int_iterator = vector_cow::Vector<int>::iterator;
+
+  // +=, +
+  {
+    vector_cow::Vector<int> v{1, 2, 3, 4, 5, 6};
+    int_iterator it = v.begin() + 2;
+    it += 1;
+    ASSERT_EQ(*it, 4);
+  }
+
+  // -=, -
+  {
+    vector_cow::Vector<int> v{1, 2, 3, 4, 5, 6};
+    int_iterator it = v.end() - 2;
+    it -= 3;
+    ASSERT_EQ(*it, 2);
+
+    int_iterator it_2 = v.end();
+    std::ptrdiff_t res = it_2 - it;
+    ASSERT_EQ(res, 5);
+  }
+
+  //[]
+  {
+    vector_cow::Vector<int> v{1, 2, 3, 4, 5, 6};
+    int_iterator it = v.begin();
+    ASSERT_EQ(it[2], 3);
+    it[2] = 4;
+    ASSERT_EQ(it[2], 4);
+  }
+
+  // compare
+  {
+    vector_cow::Vector<int> v{1, 2, 3, 4, 5, 6};
+    int_iterator it = v.begin();
+    ASSERT_EQ((it == v.begin()), true);
+    ASSERT_EQ((it == v.end() - 1), false);
+    ASSERT_EQ((it < v.begin() + 2), true);
+    ASSERT_EQ((it + 4 > v.begin() + 2), true);
+  }
+}
+
+TEST(test_vector, insert_1) {
+  {
+    vector_cow::Vector<int> v{1, 2, 3, 4};
+    vector_cow::Vector<int>::iterator it = v.insert(v.cend(), 7);
+    ASSERT_EQ(*(it), 7);
+  }
+  {
+    vector_cow::Vector<int> v(2, 100);
+    v.insert(v.begin() + 1, 200);
+    ASSERT_EQ(v, (vector_cow::Vector<int>{100, 200, 100}));
+  }
+  {
+    vector_cow::Vector<std::string> v(2, "hello");
+    v.insert(v.begin() + 1, "world");
+    ASSERT_EQ(v, (vector_cow::Vector<std::string>{"hello", "world", "hello"}));
+  }
+}
+
+TEST(test_vector, insert_2) {
+  {
+    vector_cow::Vector<int> v(2, 100);
+    auto it = v.end();
+    v.insert(it, 2, 200);
+    ASSERT_EQ(v, (vector_cow::Vector<int>{100, 100, 200, 200}));
+  }
+  {
+    vector_cow::Vector<int> v(2, 100);
+    auto it = v.begin();
+    it = v.insert(it, 2, 200);
+    ASSERT_EQ(v, (vector_cow::Vector<int>{200, 200, 100, 100}));
+  }
+}
+
+TEST(test_vector, insert_3) {
+  {
+    vector_cow::Vector<int> v(2, 100);
+    vector_cow::Vector<int> v_1(2, 200);
+    auto it = v.insert(v.end(), v_1.begin(), v_1.end());
+    ASSERT_EQ(*it, 200);
+    ASSERT_EQ(v, (vector_cow::Vector<int>{100, 100, 200, 200}));
+  }
+  {
+    vector_cow::Vector<int> v(2, 100);
+    vector_cow::Vector<int> v_1(2, 200);
+    v.insert(v.begin(), v_1.begin(), v_1.end());
+    ASSERT_EQ(v, (vector_cow::Vector<int>{200, 200, 100, 100}));
+  }
+}
+
+TEST(test_vector, insert_4) {
+  {
+    vector_cow::Vector<int> v(2, 100);
+    auto it = v.insert(v.end(), {200, 200});
+    ASSERT_EQ(*it, 200);
+    ASSERT_EQ(v, (vector_cow::Vector<int>{100, 100, 200, 200}));
+  }
+  {
+    vector_cow::Vector<int> v(2, 100);
+    v.insert(v.begin(), {200, 200});
+    ASSERT_EQ(v, (vector_cow::Vector<int>{200, 200, 100, 100}));
+  }
+}
+
+TEST(test_vector, erase) {
+  {
+    vector_cow::Vector<int> v{1, 2, 3};
+    auto it = v.erase(v.end() - 1);
+    ASSERT_EQ(it, v.end());
+    ASSERT_EQ(v, (vector_cow::Vector<int>{1, 2}));
+  }
+  {
+    vector_cow::Vector<int> v{1, 2, 3};
+    auto it = v.erase(v.end() - 2);
+    ASSERT_EQ(*it, 3);
+    ASSERT_EQ(v, (vector_cow::Vector<int>{1, 3}));
+  }
+}
+
+TEST(test_vector, erase_range) {
+  {
+    vector_cow::Vector<int> v{1, 2, 3, 4, 5};
+    auto it = v.erase(v.begin() + 1, v.end() - 1);
+    ASSERT_EQ(*it, 5);
+    ASSERT_EQ(v, (vector_cow::Vector<int>{1, 5}));
+  }
+  {
+    vector_cow::Vector<int> v{1, 2, 3, 4, 5};
+    auto it = v.erase(v.begin() + 1, v.end());
+    ASSERT_EQ(it, v.end());
+    ASSERT_EQ(v, (vector_cow::Vector<int>{1}));
+    ASSERT_EQ(v.size(), 1);
+    ASSERT_EQ(v.capacity(), 5);
+  }
+}
+
+int main(int argc, char **argv) {
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
